6d3fe69cc284841b7acac7c3b5cf862e140b9e96
lafree317 <lafree317@163.com>
2016-04-04 11:52:05 +0800

    iframe&openWindow
    这书越到后面讲的越乱啊…有时候书里只讲一半源码..剩下的得自己照着上面的补..不过也挺锻炼人的

--- LearnJavaScript/frame/02-iFrames.js
+++ LearnJavaScript/frame/02-iFrames.js
@@ -1,21 +1,34 @@
-/**
- * Created by huchunyuan on 16/4/3.
- */
-window.onload = initLinks
-var pageCount = new Array(0,0,0,0);
-function initLinks(){
-    for (var i=0;i<document.links.length;i++){
-        document.links[i].onclick = writeContent
-        document.links[i].thisPage = i+1;
+///**
+// * Created by huchunyuan on 16/4/3.
+// */
+//window.onload = initLinks
+//var pageCount = new Array(0,0,0,0);
+//function initLinks(){
+//    for (var i=0;i<document.links.length;i++){
+//        document.links[i].onclick = writeContent
+//        document.links[i].thisPage = i+1;
+//    }
+//}
+//function writeContent(){
+//    pageCount[this.thisPage]++
+//
+//    var newText = "<h1>You are now looking at Example" + this.thisPage
+//    newText += ".<br>You have been to this page"
+//    newText += pageCount[this.thisPage] + "times.<\/h1>"
+//
+//    document.getElementById("icontent").contentWindow.document.body.innerHTML = newText
+//    return false
+//}
+var bannerArray = new Array("images/redBanner.gif", "images/greenBanner.gif", "images/blueBanner.gif");
+window.onload = initLinks;
+function initLinks() {
+    for (var i=0; i<parent.document.links.length; i++) {
+        parent.document.links[i].onclick = setBanner;
     }
+    setBanner();
 }
-function writeContent(){
-    pageCount[this.thisPage]++
-
-    var newText = "<h1>You are now looking at Example" + this.thisPage
-    newText += ".<br>You have been to this page"
-    newText += pageCount[this.thisPage] + "times.<\/h1>"
-
-    document.getElementById("icontent").contentWindow.document.body.innerHTML = newText
-    return false
+function setBanner() {
+    var randomNum = Math.floor(Math.random() * bannerArray.length);
+    parent.document.getElementById("adBanner").src = bannerArray[randomNum];
+    return false;
 }
